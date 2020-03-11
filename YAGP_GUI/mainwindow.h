8959785b2ed820551887eb8a7c095274abbac8a1
Basel Shehabi <baselshehabi1@gmail.com>
2020-03-11 00:06:36 +0000

    Oops, pushed the builder/maker rather than the GUI itself!

---
+++ YAGP_GUI/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow> // Inherits QWidget. This is an upgrade in Qt 5.14
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class QPushButton;
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+signals:
+public slots:
+
+private:
+    QPushButton *m_button;
+    QPushButton *a_button;
+    QPushButton *b_button;
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
