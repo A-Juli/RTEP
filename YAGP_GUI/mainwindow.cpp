58224c1f6796ba7148d2c9d7f9a2cb8e2f0602e3
Basel Shehabi <baselshehabi1@gmail.com>
2020-04-11 11:37:16 +0100

    Started work on GUI again.

--- YAGP_GUI/mainwindow.cpp
+++ YAGP_GUI/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPushButton>
+#include <QLabel>
+#include <QHBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,16 +15,21 @@ MainWindow::MainWindow(QWidget *parent)
     m_button = new QPushButton("Start Caliberation", this);
     a_button = new QPushButton("End Caliberation", this);
     b_button = new QPushButton("Close", this);
+    label = new QLabel("Angle Reading is: ", this);
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout -> addWidget(label);
 
     m_button -> setGeometry(10, 10, 120, 30);
     a_button -> setGeometry(10, 40, 120, 30);
     b_button -> setGeometry(10, 450, 120, 30);
+    label -> setGeometry(10, 100, 120, 30);
 
     /* Attaches event handler to close window once the close button is pressed
      * More info on event handlers can be found in documentation. Use it as a
      * reference in order to generate animations, etc... it uses the signal
      * slot system which is based on the observer pattern */
     connect(b_button, SIGNAL (clicked()), QApplication::instance(), SLOT (quit()));
+    //connect(label, SIGNAL ())
 
     //ui->setupUi(this);
 }
