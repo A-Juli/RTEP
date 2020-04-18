56b0173e597191b9a4df9bf9ec22bf1cb61ef051
richardajordan <rajordan1997@gmail.com>
2020-04-18 12:32:20 +0100

    Addition of Exercise 2 and setup objects

--- Software/ExerciseTests/main.cpp
+++ Software/ExerciseTests/main.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
+#include <math.h>
 #include "ExerciseTests.h"
+
 
 using namespace std;
 
 int main()
 {
     int q = 0;
-    ExerciseTests Test;   //Declare Box1 of type box
+    ExerciseTests Test;
+    
+    // Exercise One
 
-    //Box1 Specification
     Test.setAccel1(0,0,1);
     Test.setAccel2(0,0,1);
 
     //Instead of loop us a timer to call whichever Exercise every 50ms
-    cout << "beginning of exercise 1" << endl; 
+    cout << "Beginning of Exercise 1" << endl; 
+    Test.Exercise1Setup();
+    cout << "Target Angle: " << Test.TargetAngle[0] << endl;
+
     for(q=0;q < 10;q++)
     {
         Test.Exercise1();
-        cout << "Dot Product is : " << Test.DotProduct << endl;
-        cout << "Angle between vectors: " << Test.Angle << endl;
+        cout << "Angle between vectors: " << Test.Angle[0] << endl;
     }
 
-    return 0;
+    //Exercise 2
+
+    Test.setAccel1(0,0,1);
+    Test.setAccel2(0,0,1);
+    Test.setAccel3(0,0,1);
+    Test.setAccel4(0,0,1);
+
+    cout << "Beginning of Exercise 2" << endl;
+    Test.Exercise2Setup();
+    cout << "Target Angle: " << Test.TargetAngle[0] << " : " << Test.TargetAngle[1] << " : " << Test.TargetAngle[2] << " : " << endl;
+
+    for(q=0;q < 19;q++)
+    {
+        Test.Exercise2();
+        cout << "Angle between vectors: " << Test.Angle[0] << " : " << Test.Angle[1] << " : " << Test.Angle[2] <<  endl;
+    }
 }
 
